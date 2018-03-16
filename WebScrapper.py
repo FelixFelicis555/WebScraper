c23cae6e81c73898130d5a62cba15107de532c0e
abhishek371 <kamalabhishek20@gmail.com>
2018-03-16 17:05:07 +0530

    Scraper file updated

--- WebScrapper.py
+++ WebScrapper.py
@@ -9,6 +9,46 @@ headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gec
 def parse(u):
 	return 'Parsed...{0}'.format(u)
 
-def fetch(category_url):
+#fetch method will fetch the total links and total pages in the url/webpage
 
-	
+def fetch(category_url):
+	total_pages=0
+	total_links=[]
+	try:
+		r=requests.get(category_url,headers=headers,timeout=5)
+		if r.status_code==200:
+			html=r.text
+			#print(html)
+			soup=BeautifulSoup(html,'lxml')
+			links=soup.select('.atfResults .a-link-normal')
+			for i in links:
+				print(links[0].text)
+
+
+	except requests.ConnectionError as e:
+		print("OOPS!! Conection Error. Make sure you are connected to Internet. Technical Details are : \n")
+		print(str(e)) #Error Thrown
+	except requests.Timeout as e:
+		print("OOPS!! Timeout Issue")
+		print(str(e))
+	except requests.RequestException as e:
+		print("OOPS!! General Error")
+		print(str(e))
+	except requests.KeyboardInterrupt:
+		print("Someone Closed the Program")
+
+	finally:
+		pass
+
+def main():
+	category_url="https://www.amazon.in/s/ref=lp_1389432031_nr_p_n_operating_system_0?fst=as%3Aoff&rh=n%3A976419031%2Cn%3A%21976420031%2Cn%3A1389401031%2Cn%3A1389432031%2Cp_n_operating_system_browse-bin%3A1485077031&bbn=1389432031&ie=UTF8&qid=1521195719&rnid=1485076031"
+	fetch(category_url)
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+      
+
